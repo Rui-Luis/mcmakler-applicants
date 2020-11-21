ad4c331112d4b99d916a72d2afb32eb3b178c118
Rui Luis <ruidcluis@gmail.com>
2020-11-21 00:54:32 +0000

    Minor fixes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import Search from "./Search";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function generateRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 function App() {
   return (
     <Router>
